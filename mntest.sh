a28b6824dfc41b295892e264a480ee3bd7c5acd6
Steven Farmer - Alternate <steven.farmer@mesaaz.gov>
2023-11-28 14:11:45 -0700

    updates for mesanow testing

--- mntest.sh
+++ mntest.sh
@@ -5,31 +5,34 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 # See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
+export TARGET_PROTOCOL="http"
 export TARGET_HOST="mndevl.mesanow.mesa"
 export TARGET_PORT="30080"
 export TARGET_PATH="/mapp/utilities/ping"
 export TARGET_KEYWORD="mesanow"
+export TEST_NAME="mndevl"
 
-T_DIR=tests/trivial
-
+T_DIR=tests/mesanow
 # Reporting dir: start fresh
 R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/${TEST_NAME}.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
+        -JTARGET_PROTOCOL=${TARGET_PROTOCOL} \
 	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+	-n -t ${T_DIR}/${TEST_NAME}.jmx -l ${T_DIR}/${TEST_NAME}.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
 cat ${T_DIR}/jmeter.log
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/test-plan.jtl
+cat ${T_DIR}/${TEST_NAME}.jtl
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ${R_DIR}/index.html"
+cp ${R_DIR} /mnt/image_storage/jmet -R
---
+++ setvars.sh
@@ -0,0 +1,7 @@
+export TARGET_PROTOCOL="http"
+export TARGET_HOST="mndevl.mesanow.mesa"
+export TARGET_PORT="30080"
+export TARGET_PATH="/mapp/utilities/ping"
+export TARGET_KEYWORD="mesanow"
+export TEST_NAME="mndevl"
+
