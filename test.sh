060940837e6f85d54e00267817dacb9f30d0639c
Steven Farmer - Alternate <steven.farmer@mesaaz.gov>
2023-11-27 20:28:52 -0700

    Add updated test file for mesanow

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ export TARGET_HOST="mndevl.mesanow.mesa"
 export TARGET_PORT="30080"
 export TARGET_PATH="/mapp/utilities/ping"
 export TARGET_KEYWORD="mesanow"
+export TEST_NAME="mndevl"
 
 T_DIR=tests/trivial
 
@@ -17,19 +18,19 @@ R_DIR=${T_DIR}/report
 rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+/bin/rm -f ${T_DIR}/${TEST_NAME}.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
 	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
 	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
-	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
+	-n -t ${T_DIR}/${TEST_NAME}.jmx -l ${T_DIR}/${TEST_NAME}.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
 cat ${T_DIR}/jmeter.log
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/test-plan.jtl
+cat ${T_DIR}/${TEST_NAME}.jtl
 
 echo "==== HTML Test Report ===="
 echo "See HTML test report in ${R_DIR}/index.html"
